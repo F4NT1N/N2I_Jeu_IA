2acd0d8d6d6404647323e16b10b97cb5eeb26d23
F4NT1N <130548072+F4NT1N@users.noreply.github.com>
2024-12-05 20:46:29 +0100

    refacto model

--- model.py
+++ model.py
@@ -7,16 +7,16 @@ class model:
 
     def __init__(self):
         # joueur 1, joueur2
-        player1 = Player(1)
-        player2 = Player(2)
+        self.player1 = Player(1)
+        self.player2 = Player(2)
 
-        pieces_p1 = [Piece_destructor(1, GRID_HEIGHT-1, 1), Piece_explorator(3, GRID_HEIGHT-1, 1), Piece_protector(4, GRID_HEIGHT-1, 1)] # [pions joueur1] (3)
-        pieces_p2 = [Piece_destructor(4, 0, 2), Piece_explorator(2, 0, 2), Piece_protector(1, 0, 2)]  # [pions joueur2] (3)
+        self.pieces_p1 = [Piece_destructor(1, GRID_HEIGHT-1, 1), Piece_explorator(3, GRID_HEIGHT-1, 1), Piece_protector(4, GRID_HEIGHT-1, 1)] # [pions joueur1] (3)
+        self.pieces_p2 = [Piece_destructor(4, 0, 2), Piece_explorator(2, 0, 2), Piece_protector(1, 0, 2)]  # [pions joueur2] (3)
 
         all_coordinates = [(x, y) for x in range(GRID_WIDTH) for y in range(1, GRID_HEIGHT-1)]
         selected_coordinates = random.sample(all_coordinates, 6)
-        resources = [Ressource(selected_coordinates[i][0], selected_coordinates[i][1]) for i in range(3)] # [ressources] (3)
-        zones = [Zone(selected_coordinates[i][0], selected_coordinates[i][1]) for i in range(3, 6)]# [zones] (3)
+        self.resources = [Ressource(selected_coordinates[i][0], selected_coordinates[i][1]) for i in range(3)] # [ressources] (3)
+        self.zones = [Zone(selected_coordinates[i][0], selected_coordinates[i][1]) for i in range(3, 6)]# [zones] (3)
         
         self.selected_piece = None # piece séléctionnée
         self.round = 0
--- view_controller.py
+++ view_controller.py
@@ -3,5 +3,7 @@ from model import *
 
 # importer les ressources
 
-
+pygame.init()
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Tetris")
 
