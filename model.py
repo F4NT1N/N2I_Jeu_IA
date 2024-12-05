dfe23f019b3162bdd99dfff0381bbc7ab3b56294
F4NT1N <130548072+F4NT1N@users.noreply.github.com>
2024-12-05 23:21:50 +0100

    WIP model

--- model.py
+++ model.py
@@ -22,20 +22,62 @@ class model:
         self.round = 0
 
 
-
     # choisir la case pour obtenir les coups possibles de la piece
-    def case_click(self, x, y):
-
-        # si la case cliquée possède une pièce, changer la piece selectionné. et return ses positions possibles
+    def cell_clicked(self, x, y):
         
-        if self.selected_piece != None:
+        # si la case cliquée possède une pièce du joueur, changer la piece selectionné. et return ses positions possibles
+        if self.selected_piece == None:
+            for p in self.pieces_p1 if self.round % 2 == 0 else self.pieces_p2:
+                if p.pos_x == x and p.pos_y == y:
+                    self.selected_piece = p
+                    return self.possible_moves(p)
+        else:
             # créer une liste de positions possibles de la pièces
-            # si la nouvelle position n'est pas dans les positions possibles, mettre la pièce sélectionnée à None et vider positions possibles 
-            # si la nouvelle positoin est dans les pos possibles, déplacer la pièce puis vérifier si une piece 
-            
-            pass
-        
-        return [] # renvoyer les positions possibles
+            possible_moves = self.possible_moves(self.selected_piece)
+    
+            # si la nouvelle position n'est pas dans les positions possibles, mettre la pièce sélectionnée à None et vider positions possibles
+            if (x,y) not in possible_moves:
+                self.selected_piece = None
+                return []
+            # si la nouvelle position est dans les pos possibles, faire l'action demandée
+            else:
+                if type(self.selected_piece).__name__() != "Piece_explorator":
+                    if (x,y) in self.pieces_p2 if self.round % 2 == 0 else self.pieces_p1: 
+                        # si on clique sur une piece adverse dans les pos possibles, on utilise la capacité
+                        self.use_ability()
+                    else:
+                        self.selected_piece.pos_x = x
+                        self.selected_piece.pox_y = y
+
+                self.selected_piece = None
+                return []
+    
+    
+    def possible_moves(self,piece):
+        x = piece.pos_x
+        y = piece.pos_y
+        if type(piece).__name__() == "Piece_explorator" :
+            positions = [
+                (x-1, y),(x-2, y)
+                (x+1, y),(x+2, y)
+                (x, y-1),(x, y-2)
+                (x, y+1),(x, y+2),
+                (x+1, y+1),(x+1, y-1),
+                (x-1, y+1), (x-1, y+1)
+            ]
+
+            opponent_pieces = self.pieces_p1 if round % 2 == 1 else self.pieces_p2
+            positions = [(x,y) for x, y in positions if x == p.pos_x and y == p.pos_y for p in opponent_pieces]
+        else:
+            positions = [
+                (x-1, y),
+                (x+1, y),
+                (x, y-1),
+                (x, y+1)
+            ]
+
+            return [(x, y) for x, y in positions if 0 <= x <  GRID_WIDTH and 0 <= y < GRID_HEIGHT]
+
         
 
 
--- view_controller.py
+++ view_controller.py
@@ -3,7 +3,3 @@ from model import *
 
 # importer les ressources
 
-pygame.init()
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Tetris")
-
