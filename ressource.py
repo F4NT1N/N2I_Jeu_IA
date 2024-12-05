6ff39c3934c884d86bed2d765f09cf330f8e6fda
Poux- - Bories Yoan <yoan.poux-bories@etu.iut-tlse3.fr>
2024-12-05 20:43:45 +0100

    init model

--- ressource.py
+++
@@ -1,5 +0,0 @@
-class resource_:
-
-	def __init__(self,x,y):
-		self.pos_x = x
-		self.pos_y = y
--- zone.py
+++
@@ -1,6 +0,0 @@
-class zone:
-
-	def __init__(self,x,y):
-		self.pos_x = x
-		self.pos_y = y
-		self.remain = 3
